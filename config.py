cd527e5320719f0c77091e33e350d679b897dc38
teamjassbeniwal-ctrl <teamjassbeniwal@gmail.com>
2026-02-06 20:46:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002746874071"))
-
+FORCE_CHANNELS = [-1002888391802, -1002746874071]
 
 # ==============================
 # Error Handling
